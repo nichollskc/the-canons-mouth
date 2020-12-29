ba8157770ea91b4f873558921210895333ab2fb0
Kath Nicholls <nichollskc@gmail.com>
2020-12-29 18:48:38 +0000

    Return dummy matches proportional to number of matches

--- api/routes.py
+++ api/routes.py
@@ -12,7 +12,11 @@ def process_search(encoded_request, per_page, page):
     request_b64 = base64.b64decode(encoded_request)
     request = json.loads(request_b64)
     print(request)
-    matches = [{"id": i, "match": f"{request['pattern']} {i}", "text": "The Iliad"} for i in range(20)]
+    pattern = request['pattern']
+    if len(pattern) > 0:
+        matches = [{"id": i, "match": f"{pattern} {i}", "text": "The Iliad"} for i in range(len(pattern)*7)]
+    else:
+        matches = []
 
     start_index = int(page) * int(per_page)
     end_index = start_index + int(per_page)
