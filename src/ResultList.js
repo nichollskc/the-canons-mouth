087ef14ebee6cde2ed080462a46ff38b6b4866cb
Kath Nicholls <nichollskc@gmail.com>
2021-01-01 22:34:57 +0000

    Further improvements to styling

--- src/ResultList.js
+++ src/ResultList.js
@@ -17,24 +17,39 @@ const ResultList = ({resultList=[]}) => {
     text_names.set("./texts/tokenized/paradise_regained.txt", "Milton's Paradise Regained");
     text_names.set("./texts/tokenized/mabinogion.txt", "Lady Charlotte Guest's Mabinogion");
 
+
+    const text_ids = new Map();
+    text_ids.set("./texts/tokenized/malory1.txt", "malory1");
+    text_ids.set("./texts/tokenized/malory2.txt", "malory2" );
+    text_ids.set("./texts/tokenized/shakespeare.txt", "shakespeare");
+    text_ids.set("./texts/tokenized/idylls_king.txt", "idylls_king");
+    text_ids.set("./texts/tokenized/faerie_queen.txt", "faerie_queen");
+    text_ids.set("./texts/tokenized/bible.txt", "bible");
+    text_ids.set("./texts/tokenized/aeneid.txt", "aeneid");
+    text_ids.set("./texts/tokenized/iliad.txt", "iliad");
+    text_ids.set("./texts/tokenized/odyssey.txt", "odyssey");
+    text_ids.set("./texts/tokenized/paradise_lost.txt", "paradise_lost");
+    text_ids.set("./texts/tokenized/paradise_regained.txt", "paradise_regained");
+    text_ids.set("./texts/tokenized/mabinogion.txt", "mabinogion");
+
     return (
-        <React.Fragment>
+        <div class="resultsList">
         { resultList.map((data,index) => {
             if (data) {
                 return (
-                    <div key={data.key} class="canonText">
-                    <pre>{data.before}<b>{data.matching}</b>{data.after}</pre>
-                    <div class="resultLabel">
-                        <p>{text_names.get(data.text)}{data.start_line}</p>
-                        <p>Errors:{data.errors}</p>
-                    </div>
+                    <div key={data.key} class={`canonText ${text_ids.get(data.text)}`}>
+                        <pre>{data.before}<b>{data.matching}</b>{data.after}</pre>
+                        <div class="resultLabel">
+                            <p>{text_names.get(data.text)}{data.start_line}</p>
+                            <p>Errors:{data.errors}</p>
+                        </div>
                     </div>
                 )
             }
             return null
         })
         }
-        </React.Fragment>
+        </div>
     );
 }
 
