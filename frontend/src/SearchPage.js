f3f728990c572f75e213049973530b7e7a632566
Kath Nicholls <nichollskc@gmail.com>
2021-01-09 12:54:53 +0000

    Add html elements to set cconfig for search

--- frontend/src/SearchPage.js
+++ frontend/src/SearchPage.js
@@ -18,6 +18,9 @@ const SearchPage = (props) => {
     const [keyword, setKeyword] = useState('');
     const [searchKeyword, setSearchKeyword] = useState('');
     const [caseInsensitive, setCaseInsensitive] = useState(true);
+    const [maxErrors, setMaxErrors] = useState('0');
+    const [ignoredCharacters, setIgnoredCharacters] = useState(';",.');
+
     const [resultList, setResultList] = useState();
     const [promiseInProgress, setPromiseInProgress] = useState(false);
     const [promiseBegun, setPromiseBegun] = useState(false);
@@ -25,14 +28,14 @@ const SearchPage = (props) => {
     const delayedSetKeyword = useRef(debounce((newValue) => setSearchKeyword(newValue), 1000));
     const delayedSetPromiseInProgress = useRef(debounce((newValue) => setPromiseInProgress(newValue), 1));
 
-    const updateResults = async (searchKeyword, currentPage, caseInsensitive) => {
+    const updateResults = async (searchKeyword, currentPage, caseInsensitive, maxErrors, ignoredCharacters) => {
         console.log(props.perPage);
         let obj = {
                   'pattern': searchKeyword,
                   'config': {'selected_texts': Array.from(props.selected_texts),
-                             'max_errors': 1,
+                             'max_errors': parseInt(maxErrors),
                              'case_insensitve': caseInsensitive,
-                             'ignored_chars': ';:'},
+                             'ignored_chars': ignoredCharacters },
         };
         console.log(obj);
         let objJsonStr = JSON.stringify(obj);
@@ -63,8 +66,8 @@ const SearchPage = (props) => {
     // Delay this request until no keypresses have been made in the last second to
     // avoid sending too many requests
     useEffect(() => {
-        updateResults(searchKeyword, currentPage, caseInsensitive)
-    }, [searchKeyword, currentPage, props.selected_texts, caseInsensitive]);
+        updateResults(searchKeyword, currentPage, caseInsensitive, maxErrors, ignoredCharacters)
+    }, [searchKeyword, currentPage, props.selected_texts, caseInsensitive, maxErrors, ignoredCharacters]);
 
     useEffect(() => {
         delayedSetPromiseInProgress.current(promiseBegun)
@@ -88,10 +91,25 @@ const SearchPage = (props) => {
                     <SearchBar keyword={keyword} setKeyword={updateKeyword} />
                 </Col>
                 <Col>
-                    <Checkbox name="case_insensitive" checked={caseInsensitive} onChange={handleCaseCheckboxChange} />
+                    <LoadingSpinner promiseInProgress={promiseInProgress}/>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <label key="case_insensitive_checkbox">
+                        <Checkbox name="case_insensitive" checked={caseInsensitive} onChange={handleCaseCheckboxChange} />
+                        <span> Case insensitive</span>
+                    </label>
                 </Col>
                 <Col>
-                    <LoadingSpinner promiseInProgress={promiseInProgress}/>
+                    <span>Errors allowed </span>
+                    <input id="max_errors" size="1" value={maxErrors} onChange={(e) => setMaxErrors(e.target.value.replace(/\D/,''))} />
+                </Col>
+                <Col>
+                    <label>
+                        <span>Ignore </span>
+                        <input id="ignored_characters" size="4" value={ignoredCharacters} onChange={(e) => setIgnoredCharacters(e.target.value)} />
+                    </label>
                 </Col>
             </Row>
             <Row>
