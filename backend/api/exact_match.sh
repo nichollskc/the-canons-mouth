80a007d2aec37f7498d62c43cc282f8ac1980cba
Kath Nicholls <nichollskc@gmail.com>
2021-02-05 17:44:12 +0000

    Improve exact awk script - deal nicely with multiple matches

--- backend/api/exact_match.sh
+++ backend/api/exact_match.sh
@@ -4,12 +4,23 @@ PATTERN=$1
 FILENAME=$2
 
 awk -v PATTERN="$PATTERN" -v FILENAME="$FILENAME" \
-    'BEGIN { OFS="==="; ORS="\nEND\n"}
-           { NS==split($0, line_parts, PATTERN, seps);
-             for (i in seps)
+    'function join_with_seps(array, separray, start, end,     result, j)
+     {
+         result = array[start]
+         for (j = start + 1; j <= end; j++)
+             result = result separray[j - 1] array[j]
+         return result
+     }
+
+     BEGIN { OFS="===";
+             ORS="\nMATCHEND\n"}
+           { NS=split($0, line_parts, PATTERN, seps);
+             for (i in seps) {
                  print FILENAME,
-                       line_parts[i],
+                       join_with_seps(line_parts, seps, 1, i, PATTERN),
                        seps[i],
-                       line_parts[i+1],
-                       NR-1
+                       join_with_seps(line_parts, seps, i+1, NS, PATTERN),
+                       NR - 1,
+                       NR - 1;
+             }
            }' $FILENAME
